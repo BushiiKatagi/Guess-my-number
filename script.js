a984349ac7e7bf975bab628e7d434d027521adfd
Antonio Borojevic <antonio.borojevic@gmail.com>
2023-02-01 22:11:05 +0100

    finish

--- script.js
+++ script.js
@@ -79,6 +79,8 @@ document.querySelector('.check').addEventListener('click', function () {
   // }
 });
 
+// Coding Challenge #1
+
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
