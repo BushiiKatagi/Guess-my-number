1fb76aebbbe6cbf75ad93a677471349ab51b10e7
Antonio Borojevic <antonio.borojevic@gmail.com>
2023-02-06 21:27:26 +0100

    fixing problem

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.querySelector('.guess').value = 13;
 console.log(document.querySelector('.guess').value);
 */
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 // document.querySelector('.number').textContent = secretNumber;
